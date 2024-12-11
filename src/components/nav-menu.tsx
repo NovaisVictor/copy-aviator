4b4cb66fed3c97a9a85c458c97d3f926e69f6b9b
NovaisVictor <victornovaislima@gmail.com>
2024-12-11 11:52:15 -0300

    nav-menu updates

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -7,8 +7,8 @@ import { usePathname } from 'next/navigation'
 export function NavMenu() {
   const pathName = usePathname()
   return (
-    <div className="fixed bottom-0 right-0 left-0">
-      <div className="flex justify-around items-center bg-primary-foreground p-1 pb-4 uppercase font-semibold text-sm">
+    <div className="fixed bottom-2 right-2 left-2 z-[100000]">
+      <div className="flex justify-around rounded-full border border-primary items-center bg-accent p-1 pb-4 uppercase font-semibold text-sm">
         <div className="flex flex-col items-center justify-center space-y-0">
           <Link
             href={'/'}
